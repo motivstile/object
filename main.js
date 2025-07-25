37f02a55661d8714572c780575d5dfdca6324092
motivstile <amirbekufc1@gmail.com>
2025-07-25 15:31:19 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let username = prompt("Ismingiznini kiriting")
+let spaceId = prompt("Space id kiriting")
+let password = prompt("Parol kiriting")
+// const btn = document.querySelector('.red')
+// console.log(btn)
+// const body = document.querySelector('body')
+// btn.addEventListener("click", () => {
+//     body.style.background = "red"
+// })
+
+const user = {
+    name: username,
+    spaceId: spaceId,
+    password: password 
+}
+console.log(user)
